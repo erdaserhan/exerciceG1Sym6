406d14d914415193afdd8b708ede7ae0fdf09072
erdaserhan <erhan209@gmail.com>
2024-09-26 16:08:05 +0200

    composer updated

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,9 @@
 import './bootstrap.js';
+import './js/jquery.ajaxchimp.min.js';
+import './js/jquery.form.js';
+import './js/jquery.validate.min.js';
+import './js/mail-script.js';
+import './js/main.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,5 +11,7 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/main.css';
+import './styles/style.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
