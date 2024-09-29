7f1269885810b9c00b4606ecd0230729abf0116e
erdaserhan <erhan209@gmail.com>
2024-09-29 20:10:28 +0200

    section crud created

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,17 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Form\PostType;
+use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
         return $this->render('main/index.html.twig', [
             'title' => 'Homepage',
             'homepage_text'=> "Nous somme le ".date('d/m/Y \à H:i'),
+            'posts' => $postRepository->findAll(),
         ]);
     }
 }
