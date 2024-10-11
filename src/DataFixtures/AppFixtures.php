1622ab7bc86797fb51280b394a4bf8c30a3ebdc1
erdaserhan <erhan209@gmail.com>
2024-10-11 13:58:10 +0200

    one user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,22 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+# On va récupérer notre entité User
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setUserMail('admin@gmail.com');
+        $user->setRoles(['ROLE_ADMIN', 'ROLE_REDAC', 'ROLE_MODERATOR']);
+        $user->setPassword('admin');
+        $user->setUserActive(true);
+        $user->setUserRealName('The Admin !');
+
+        $manager->persist($user);
 
         $manager->flush();
     }
