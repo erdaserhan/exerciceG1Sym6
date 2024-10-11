bde21ba058447415e2a3106abbeef7fae7cc2307
erdaserhan <erhan209@gmail.com>
2024-10-11 15:50:01 +0200

    fixtures users ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,8 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 # On va hasher les mot de passe
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+# Chargement de Faker et création d'un alias nommé Faker
+use Faker\Factory as Faker;
 # On va récupérer notre entité User
 use App\Entity\User;
 
@@ -34,8 +36,6 @@ class AppFixtures extends Fixture
         $manager->persist($user);
 
 
-
-
         # Instanciation de 5 Rédacteurs
         for ($i = 1; $i <= 5; $i++) {
             $user = new User();
@@ -50,7 +50,7 @@ class AppFixtures extends Fixture
             $manager->persist($user);
         }
 
-        # Instanciation de 5 Moderators
+        # Instanciation de 3 Moderators
         for ($i = 1; $i <= 3; $i++) {
             $user = new User();
             $user->setUsername('moderator'.$i);
@@ -64,23 +64,32 @@ class AppFixtures extends Fixture
             $manager->persist($user);
         }
 
+        //Instanciation de Faker en français
+        $faker = Faker::create('fr_FR');
 
-        # Instanciation de 5 Users
-        for ($i = 1; $i <= 10; $i++) {
+        # Instanciation entre 20 et 40 User sans rôles en utilisant Faker
+        $hasard = mt_rand(20,40);
+        for ($i = 1; $i <= $hasard; $i++) {
             $user = new User();
-            $user->setUsername('moderator'.$i);
-            $user->setUserMail('moderator'.$i.'@gmail.com');
-            $user->setRoles(['ROLE_MODERATOR']);
-            $pwdHash = $this->passwordHasher->hashPassword($user, 'moderator'.$i);
+            //nom d'utilisateur au hasard commençant pas user-1234
+            $username = $faker->numerify('user-####');
+            $user->setUsername($username);
+            // Création d'un mail au hasard
+            $mail = $faker->email();
+            $user->setUserMail($mail);
+            $user->setRoles(['ROLE_USER']);
+            //Transformation du nom en mot de passe
+            #(pour tester)
+            $pwdHash = $this->passwordHasher->hashPassword($user, $username);
             $user->setPassword($pwdHash);
-            $user->setUserActive(true);
-            $user->setUserRealName('The Moderator '.$i);
-
+            # On va activer 1 user sur 3
+            $randActive = mt_rand(0,2);
+            $user->setUserActive($randActive);
+            $realName = $faker->name();
+            $user->setUserRealName($realName);
             $manager->persist($user);
         }
 
-
-
-        $manager->flush();
+            $manager->flush();
     }
 }
